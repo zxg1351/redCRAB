d136a4255e48d19ad4e59b041349cbb8edab30e3
zxg1351 <zxg_546789@163.com>
2017-10-21 16:32:55 +0800

    20171021

--- src/main/java/com/redofmaple/DemoApplication.java
+++ src/main/java/com/redofmaple/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.redofmaple;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+@MapperScan(basePackages = "com.redofmaple.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/redofmaple/controller/UserController.java
+++ src/main/java/com/redofmaple/controller/UserController.java
@@ -2,11 +2,13 @@ package com.redofmaple.controller;
 
 
 import com.redofmaple.domain.Users;
+import com.redofmaple.entity.gen.MUser;
+import com.redofmaple.entity.gen.MUserExample;
+import com.redofmaple.mapper.MUserMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,10 @@ import java.util.List;
 @Api(value = "用户")
 public class UserController {
 
+    @Autowired
+    private MUserMapper mUserMapper;
+
+
     @ApiOperation(value = "获取用户列表", notes = "")
     @RequestMapping(value = {""}, method = RequestMethod.GET)
     public List<Users> getUserList() {
@@ -23,6 +29,17 @@ public class UserController {
         return r;
     }
 
+    @ApiOperation(value = "获取test数据库列表", notes = "")
+    @PostMapping("/getUserList")
+    public List<MUser> getUser() {
+        List<MUser> mUserList = new ArrayList<>();
+        MUserExample mUserExample = new MUserExample();
+        mUserExample.createCriteria().andDelFlagEqualTo("0");
+
+        mUserList = mUserMapper.selectByExample(mUserExample);
+        return mUserList;
+
+    }
 
 }
 
